c20ad5911454423be1ac6a5a748c1a83c4f2d52f
zhouna <zna1121@163.com>
2021-04-13 14:59:26 +0800

    暂存提交

--- src/page/Hooks/index.js
+++ src/page/Hooks/index.js
@@ -30,6 +30,12 @@ function About(props) {
 
     const [count1, setCount1] = useState(0)
     const [count2, setCount2] = useState(0)
+    console.log('render====', props)
+
+    useEffect(() => {
+        console.log('useEffect--')
+    }, [selfObj.age])
+
     // useEffect(() => {
     //     clearTimeout(timer.current)
     //     timer.current = setTimeout(() => {
@@ -37,215 +43,215 @@ function About(props) {
     //     }, 1000)
     // }, [count])
 
-    useEffect(() => {
-        // console.log("ref:deom-init", domRef, domRef.current);
-        // console.log("ref:child-init", childRef, childRef.current);
-        // console.log(window.location, '--location--')
-        // console.log(window.location.search, '--search--')
+    // useEffect(() => {
+    // console.log("ref:deom-init", domRef, domRef.current);
+    // console.log("ref:child-init", childRef, childRef.current);
+    // console.log(window.location, '--location--')
+    // console.log(window.location.search, '--search--')
 
-        // Object.prototype.a = "jing1";
-        // Function.prototype.a = "jing2";
-        // function Preson() { };
-        // var p = new Preson();
-        // console.log(p.a); // jing1
-        // console.log(p.constructor); // ƒ function Preson() { };
-        // console.log(p.__proto__.__proto__.constructor); // ƒ Object() { [native code] }
+    // Object.prototype.a = "jing1";
+    // Function.prototype.a = "jing2";
+    // function Preson() { };
+    // var p = new Preson();
+    // console.log(p.a); // jing1
+    // console.log(p.constructor); // ƒ function Preson() { };
+    // console.log(p.__proto__.__proto__.constructor); // ƒ Object() { [native code] }
 
-        // console.log('1');
-        // setTimeout(function () {
-        //     console.log('2');
-        //     new Promise(function (resolve) {
-        //         console.log('3');
-        //         resolve();
-        //     }).then(function () {
-        //         console.log('4')
-        //     })
-        // })
-        // new Promise(function (resolve) {
-        //     console.log('5');
-        //     resolve();
-        // }).then(function () {
-        //     console.log('6')
-        // })
-        // console.log('7')
+    // console.log('1');
+    // setTimeout(function () {
+    //     console.log('2');
+    //     new Promise(function (resolve) {
+    //         console.log('3');
+    //         resolve();
+    //     }).then(function () {
+    //         console.log('4')
+    //     })
+    // })
+    // new Promise(function (resolve) {
+    //     console.log('5');
+    //     resolve();
+    // }).then(function () {
+    //     console.log('6')
+    // })
+    // console.log('7')
 
-        // setTimeout(function () {
-        //     console.log('1');
-        // })
-        // new Promise(function (resolve) {
-        //     console.log('2');
-        //     for (var i = 0; i < 1000; i++) {
-        //         i == 99 && resolve();
-        //     }
-        //     console.log('3');
-        // }).then(function () {
-        //     console.log('4');
-        // })
-        // console.log('5')
+    // setTimeout(function () {
+    //     console.log('1');
+    // })
+    // new Promise(function (resolve) {
+    //     console.log('2');
+    //     for (var i = 0; i < 1000; i++) {
+    //         i == 99 && resolve();
+    //     }
+    //     console.log('3');
+    // }).then(function () {
+    //     console.log('4');
+    // })
+    // console.log('5')
 
-        // console.log('start')
-        // setTimeout(() => {
-        //     console.log('setTimeout')
-        // }, 0)
-        // new Promise((resolve) => {
-        //     console.log('promise')
-        //     resolve()
-        // })
-        //     .then(() => {
-        //         console.log('then1')
-        //     })
-        //     .then(() => {
-        //         console.log('then2')
-        //     })
-        // console.log('end')
+    // console.log('start')
+    // setTimeout(() => {
+    //     console.log('setTimeout')
+    // }, 0)
+    // new Promise((resolve) => {
+    //     console.log('promise')
+    //     resolve()
+    // })
+    //     .then(() => {
+    //         console.log('then1')
+    //     })
+    //     .then(() => {
+    //         console.log('then2')
+    //     })
+    // console.log('end')
 
-        // console.log('start')
-        // setTimeout(() => {
-        //     console.log('timer1')
-        //     Promise.resolve().then(function () {
-        //         console.log('promise1')
-        //     })
-        // }, 0)
-        // setTimeout(() => {
-        //     console.log('timer2')
-        //     Promise.resolve().then(function () {
-        //         console.log('promise2')
-        //     })
-        // }, 0)
-        // Promise.resolve().then(function () {
-        //     console.log('promise3')
-        // })
-        // console.log('end')
+    // console.log('start')
+    // setTimeout(() => {
+    //     console.log('timer1')
+    //     Promise.resolve().then(function () {
+    //         console.log('promise1')
+    //     })
+    // }, 0)
+    // setTimeout(() => {
+    //     console.log('timer2')
+    //     Promise.resolve().then(function () {
+    //         console.log('promise2')
+    //     })
+    // }, 0)
+    // Promise.resolve().then(function () {
+    //     console.log('promise3')
+    // })
+    // console.log('end')
 
-        // setTimeout(function () {
-        //     console.log('定时器开始啦')
-        // });
-        // new Promise(function (resolve) {
-        //     console.log('马上执行for循环啦');
-        //     for (var i = 0; i < 10000; i++) {
-        //         i == 99 && resolve();
-        //     }
-        // }).then(function () {
-        //     console.log('执行then函数啦')
-        // });
-        // console.log('代码执行结束');
+    // setTimeout(function () {
+    //     console.log('定时器开始啦')
+    // });
+    // new Promise(function (resolve) {
+    //     console.log('马上执行for循环啦');
+    //     for (var i = 0; i < 10000; i++) {
+    //         i == 99 && resolve();
+    //     }
+    // }).then(function () {
+    //     console.log('执行then函数啦')
+    // });
+    // console.log('代码执行结束');
 
-        // console.log('script start')
-        // async function async1() {
-        //     await async2()
-        //     console.log('async1 end')
-        // }
-        // async function async2() {
-        //     console.log('async2 end')
-        // }
-        // async1()
-        // setTimeout(function () {
-        //     console.log('setTimeout')
-        // }, 0)
-        // new Promise(resolve => {
-        //     console.log('Promise')
-        //     resolve()
-        // }).then(function () {
-        //     console.log('promise1')
-        // }).then(function () {
-        //     console.log('promise2')
-        // })
-        // console.log('script end')
-        // script start  async2 end  Promise  script end   async1 end  promise1 promise2 setTimeout
-        // 旧版输出如下，但是请继续看完本文下面的注意那里，新版有改动
-        // script start => async2 end => Promise => script end => promise1 => promise2 => async1 end => setTimeout
+    // console.log('script start')
+    // async function async1() {
+    //     await async2()
+    //     console.log('async1 end')
+    // }
+    // async function async2() {
+    //     console.log('async2 end')
+    // }
+    // async1()
+    // setTimeout(function () {
+    //     console.log('setTimeout')
+    // }, 0)
+    // new Promise(resolve => {
+    //     console.log('Promise')
+    //     resolve()
+    // }).then(function () {
+    //     console.log('promise1')
+    // }).then(function () {
+    //     console.log('promise2')
+    // })
+    // console.log('script end')
+    // script start  async2 end  Promise  script end   async1 end  promise1 promise2 setTimeout
+    // 旧版输出如下，但是请继续看完本文下面的注意那里，新版有改动
+    // script start => async2 end => Promise => script end => promise1 => promise2 => async1 end => setTimeout
 
 
-        // console.log('script start')
-        // async function async1() {
-        //     await async2()
-        //     console.log('async1 end')
-        // }
-        // async function async2() {
-        //     console.log('async2 end')
-        //     return Promise.resolve().then(() => {
-        //         console.log('async2 end1')
-        //     })
-        // }
-        // async1()
-        // setTimeout(function () {
-        //     console.log('setTimeout')
-        // }, 0)
-        // new Promise(resolve => {
-        //     console.log('Promise')
-        //     resolve()
-        // }).then(function () {
-        //     console.log('promise1')
-        // }).then(function () {
-        //     console.log('promise2')
-        // })
-        // console.log('script end')
-        // script start  async2 end    Promise   script end   async2 end1    promise1  promise2  async1 end  setTimeout
+    // console.log('script start')
+    // async function async1() {
+    //     await async2()
+    //     console.log('async1 end')
+    // }
+    // async function async2() {
+    //     console.log('async2 end')
+    //     return Promise.resolve().then(() => {
+    //         console.log('async2 end1')
+    //     })
+    // }
+    // async1()
+    // setTimeout(function () {
+    //     console.log('setTimeout')
+    // }, 0)
+    // new Promise(resolve => {
+    //     console.log('Promise')
+    //     resolve()
+    // }).then(function () {
+    //     console.log('promise1')
+    // }).then(function () {
+    //     console.log('promise2')
+    // })
+    // console.log('script end')
+    // script start  async2 end    Promise   script end   async2 end1    promise1  promise2  async1 end  setTimeout
 
-        // setImmediate(() => {
-        //     console.log(1)
-        //     setTimeout(() => {
-        //         console.log(2)
-        //     }, 100)
-        //     setImmediate(() => {
-        //         console.log(3)
-        //     })
-        //     process.nextTick(() => {
-        //         console.log(4)
-        //     })
-        // })
-        // process.nextTick(() => {
-        //     console.log(5)
-        //     setTimeout(() => {
-        //         console.log(6)
-        //     }, 100)
-        //     setImmediate(() => {
-        //         console.log(7)
-        //     })
-        //     process.nextTick(() => {
-        //         console.log(8)
-        //     })
-        // })
-        // console.log(9)
-        // 9 5 8 1 4 7 3 6 2
+    // setImmediate(() => {
+    //     console.log(1)
+    //     setTimeout(() => {
+    //         console.log(2)
+    //     }, 100)
+    //     setImmediate(() => {
+    //         console.log(3)
+    //     })
+    //     process.nextTick(() => {
+    //         console.log(4)
+    //     })
+    // })
+    // process.nextTick(() => {
+    //     console.log(5)
+    //     setTimeout(() => {
+    //         console.log(6)
+    //     }, 100)
+    //     setImmediate(() => {
+    //         console.log(7)
+    //     })
+    //     process.nextTick(() => {
+    //         console.log(8)
+    //     })
+    // })
+    // console.log(9)
+    // 9 5 8 1 4 7 3 6 2
 
-        console.log('1');
-        setTimeout(function () {
-            console.log('2');
-            process.nextTick(function () {
-                console.log('3');
-            })
-            new Promise(function (resolve) {
-                console.log('4');
-                resolve();
-            }).then(function () {
-                console.log('5')
-            })
-        })
-        process.nextTick(function () {
-            console.log('6');
-        })
-        new Promise(function (resolve) {
-            console.log('7');
-            resolve();
-        }).then(function () {
-            console.log('8')
-        })
-        setTimeout(function () {
-            console.log('9');
-            process.nextTick(function () {
-                console.log('10');
-            })
-            new Promise(function (resolve) {
-                console.log('11');
-                resolve();
-            }).then(function () {
-                console.log('12')
-            })
-        })
-        // 1 7 6 8 2 4 3 5 9 11 10 12 
+    //     console.log('1');
+    //     setTimeout(function () {
+    //         console.log('2');
+    //         process.nextTick(function () {
+    //             console.log('3');
+    //         })
+    //         new Promise(function (resolve) {
+    //             console.log('4');
+    //             resolve();
+    //         }).then(function () {
+    //             console.log('5')
+    //         })
+    //     })
+    //     process.nextTick(function () {
+    //         console.log('6');
+    //     })
+    //     new Promise(function (resolve) {
+    //         console.log('7');
+    //         resolve();
+    //     }).then(function () {
+    //         console.log('8')
+    //     })
+    //     setTimeout(function () {
+    //         console.log('9');
+    //         process.nextTick(function () {
+    //             console.log('10');
+    //         })
+    //         new Promise(function (resolve) {
+    //             console.log('11');
+    //             resolve();
+    //         }).then(function () {
+    //             console.log('12')
+    //         })
+    //     })
+    //     // 1 7 6 8 2 4 3 5 9 11 10 12 
 
-    }, []);
+    // }, []);
 
     const showChild = () => {
         console.log("ref:child", childRef, childRef.current);
@@ -271,21 +277,25 @@ function About(props) {
                 <Child ref={childRef} />
                 <Child1 />
             </div>
-            <br />
-            <p>{count}</p>
+            <br /> */}
+            <p>{count1}</p>
             <p onClick={() => {
-                setCount1(count1 + 1)
+                setCount1(x => x + 1)
+                setCount2(x => x + 1)
             }}>click me1</p>
+            <br />
+            <p>{count2}</p>
             <p onClick={() => {
-                setCount2(count2 + 1)
-            }}>click me</p> */}
+                setSelfObj({ name: '11', age: 20 })
+            }}>click me</p>
 
-            <nav>nav</nav>
+            {/* <nav>nav</nav>
             <main>main</main>
-            <aside>aside</aside>
+            <aside>aside</aside> */}
         </div>
     );
 }
+// export default About;
 
 const mapState = (state) => ({
     home: state.home
